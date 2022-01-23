bf925bc6b63bad758f5cad30cc6af335bd1bd4eb
Luka Cabraja <luka@cabraja.eu>
2022-01-23 03:07:44 +0100

    init commit

--- Shared/InfoedukaApp.swift
+++ Shared/InfoedukaApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct InfoedukaApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainView()
         }
     }
 }
